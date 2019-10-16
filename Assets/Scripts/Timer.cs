a8b657b1a3fb5daeac767738f57f6893094d8eda
leoneljerez <leoneljerez1022@gmail.com>
2019-10-16 13:21:59 -0400

    -Added Timer -Score label moved a little -Added back missing Pac-Dot

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private Text timetext = null;
+
+    private float seconds;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerController.performed)
+        {
+            seconds += Time.deltaTime;
+        
+            TimeSpan timeSpan = TimeSpan.FromSeconds(seconds);
+            string currTime = $"{timeSpan.Minutes:D2}:{timeSpan.Seconds:D2}";
+            timetext.text = currTime;
+        }
+       
+    }
+}
