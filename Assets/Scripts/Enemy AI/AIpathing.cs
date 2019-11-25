502c5de92534b926646afe73661b69f1fba9660f
Marcus L <mlofton4@uncc.edu>
2019-11-25 14:22:52 -0500

    Gave Blinky the A* method too, might need Collision events with the enemies colliding with the objects

---
+++ Assets/Scripts/Enemy AI/AIpathing.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AIpathing : MonoBehaviour
+{
+    [SerializeField] private Transform trans;
+    [SerializeField] public Transform[] waypoints;
+    int curW = 0;
+    public float speed = 2f;
+
+    void Update ()
+    {
+        if (trans.position != waypoints[curW].position)
+        {
+            trans.position = Vector3.MoveTowards(trans.position, waypoints[curW].position, speed * Time.deltaTime);
+
+        }
+
+        else
+        {
+            curW = (curW + 1) % waypoints.Length;
+          
+        }
+    
+    }
+}
