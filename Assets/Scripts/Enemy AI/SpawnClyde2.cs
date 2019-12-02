8639f6d415957a89be6bf10c3e079cd70f7b8c0f
Marcus L <mlofton4@uncc.edu>
2019-12-01 23:24:48 -0500

    Added another green enemy as well as a second clyde. More walls disappear as well

---
+++ Assets/Scripts/Enemy AI/SpawnClyde2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnClyde2 : MonoBehaviour
+{
+    public GameObject Clyde;
+
+
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn()
+    {
+        yield return new WaitForSeconds(70f);
+
+        Instantiate(Clyde, new Vector3(-0.41f, 1f, -23f), Quaternion.identity);
+    }
+
+}
---
+++ Assets/Scripts/Enemy AI/SpawnGreen2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnGreen2 : MonoBehaviour
+{
+    public GameObject GreenEnemy;
+
+
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn()
+    {
+        yield return new WaitForSeconds(60f);
+
+        Instantiate(GreenEnemy, new Vector3(-22f, 1f, -3.3f), Quaternion.identity);
+    }
+
+}
+
